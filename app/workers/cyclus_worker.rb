f2c74e0da0939f6f275dbf1332867e3fa8bae38e
Dave Handy <david.n.handy@gmail.com>
2017-06-22 07:18:15 -0600

    Got rails running

--- app/workers/cyclus_worker.rb
+++ app/workers/cyclus_worker.rb
@@ -1,19 +1,6 @@
 class CyclusWorker
     include Sidekiq::Worker
 
-    Sidekiq.configure_server do |config|
-      config.redis = {
-        namespace: 'cyclus',
-        url: 'redis://redis:6379/0'
-      }
-    end
-
-    Sidekiq.configure_client do |config|
-      config.redis = {
-        namespace: 'cyclus',
-        url: 'redis://redis:6379/0'
-      }
-    end
 
     def perform(job_id)
       puts "The jobID was #{job_id}"
